f2e38514d29ff4307f51664f030d1837a33ecd1c
TrulyTHRiO <55783018+TrulyTHRiO@users.noreply.github.com>
2023-07-31 22:15:02 +0100

    a b jamona

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+const lyricContainer = document.getElementById("lyric-container")
 const inputField = document.getElementById("input-container")
 const lyricInput = document.getElementById("lyric-input")
 
-lyricInput.oninput = function() {
-    console.log(lyricInput.value)
+// var lyrics = targetSong.lyrics.split(" ")
+var lyrics = ["a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", "a", "b", "jamona", ]
+var unpunctuatedLyrics = []
+
+lyrics.forEach(function(lyric) {
+    createLyric(lyric)
+    unpunctuatedLyrics.push(lyric.replaceAll(/[^A-Za-z]/g, ""))
+})
+
+function createLyric(lyric) {
+    let lyricDOM = document.createElement("lyric")
+    lyricDOM.innerHTML = lyric
+    lyricContainer.appendChild(lyricDOM)
 }
+
+function UnhideLyric(lyric) {
+    while (unpunctuatedLyrics.indexOf(lyric) != -1) {
+        let index = unpunctuatedLyrics.indexOf(lyric)
+        unpunctuatedLyrics[index] = null
+        lyricContainer.children[index].style = "color: white"
+    }
+}
+
+lyricInput.oninput = function() {
+    let inputString = lyricInput.value.replaceAll(/[^A-Za-z]/g, "")
+    UnhideLyric(inputString)
+    console.log(inputString)
+}
+
+window.onload = () => {lyricInput.oninput()}
